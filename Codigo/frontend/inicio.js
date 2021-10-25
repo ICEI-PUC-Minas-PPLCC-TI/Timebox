29088913e265e56364b99475cb4879ff29f2fbae
Lexizz7 <guetron@gmail.com>
2021-10-25 11:53:43 -0300

    template estatico pronto

--- Codigo/frontend/inicio.js
+++ Codigo/frontend/inicio.js
@@ -1,10 +1,16 @@
+const main = document.querySelector('#main')
+
 // Menu
 const menu = document.querySelector('#menu')
-const button = document.querySelector('#abrir-menu')
+const button = document.querySelector('#menu-button')
 let opened = false
+
 const toggleMenu = () => {
     opened = !opened
     menu.setAttribute('data-opened', opened ? `true` : `false`)
     button.style.transform = opened ? `rotate(180deg)` : `rotate(0deg)`
+    menu.style.width = opened ? `250px` : `0px`
+    main.style.marginLeft = opened ? `250px` : `0px`
+
 }
 button.addEventListener('mousedown', toggleMenu)
