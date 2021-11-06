81767135f93bc8f5b2675942832e8c4b59fc27f6
Lexizz7 <guetron@gmail.com>
2021-11-06 18:57:52 -0300

    template completo (vazio)

--- Codigo/frontend/rotina.js
+++ Codigo/frontend/rotina.js
@@ -122,3 +122,26 @@ const createTextField = () => {
 }
 createTaskButton.addEventListener('click', createTask)
 createTextButton.addEventListener('click', createTextField)
+
+//Notificações
+const notificacoesButton = document.querySelector('#notification-btn')
+const openNotifications = () => {
+    const popupDiv = document.querySelector('.notifications')
+
+    const close = (mouse) => {
+        if (mouse.target != document.querySelector('.notification-content')) {
+            popupDiv.classList.remove('opened')
+        }
+    }
+
+    setTimeout(() => {
+        if (!popupDiv.classList.contains('opened')) {
+            popupDiv.classList.add('opened')
+        }
+    }, 1)
+
+
+    window.addEventListener('click', close)
+}
+
+notificacoesButton.addEventListener('click', openNotifications)
